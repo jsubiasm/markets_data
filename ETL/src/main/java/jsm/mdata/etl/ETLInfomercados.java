7446eb4ca13edd3d8dfae584f3fd8782ac72545d
jsubiasm <jsubiasm@yahoo.es>
2017-10-04 09:55:14 +0200

    Commit inicial ETL

---
+++ ETL/src/main/java/jsm/mdata/etl/ETLInfomercados.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package jsm.mdata.etl;
+
+/**
+ * @author Empleado
+ *
+ */
+public class ETLInfomercados
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
