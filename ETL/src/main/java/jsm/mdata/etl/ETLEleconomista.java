c1395f1487effd74e4a17b15b431dc91debb3a35
jsubiasm <jsubiasm@yahoo.es>
2017-10-05 11:32:48 +0200

    Importando datos desde eleconomista

---
+++ ETL/src/main/java/jsm/mdata/etl/ETLEleconomista.java
@@ -0,0 +1,185 @@
+/**
+ * 
+ */
+package jsm.mdata.etl;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Empleado
+ *
+ */
+public class ETLEleconomista
+{
+
+	/**
+	 * Logger
+	 */
+	private final static Logger LOGGER = LoggerFactory.getLogger(ETLEleconomista.class);
+
+	/**
+	 * Proxy
+	 */
+	private static final Boolean USE_PROXY = true;
+	private static final String PROXY_URL = "192.6.2.109";
+	private static final Integer PROXY_PORT = 81;
+	private static final String PROXY_USERNAME = "panda";
+	private static final String PROXY_PASSWORD = "panda";
+
+	/**
+	 * Ficheros
+	 */
+	private static final String DATA_URLS_FILE = "C:\\_PELAYO\\Software\\Eclipse Neon\\workspace\\markets_data\\ETL\\consultas\\eleconomista\\consultas.txt";
+	private static final String TMP_DATA_FILE_PATH = "C:\\_PELAYO\\Software\\Eclipse Neon\\workspace\\markets_data\\ETL\\consultas\\eleconomista\\download\\";
+	private static final String TMP_DATA_FILE_PREFIX = "_data_file_";
+
+	/**
+	 * Formatos fichero consultas
+	 */
+	private static final String CCOMENT = "--";
+	private static final String CSEPARADOR = "##CSEP##";
+	private static final String CFECINI = "##FINI##";
+	private static final String CFECFIN = "##FFIN##";
+	// private static final SimpleDateFormat CFECFORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+	/**
+	 * Conexión a base de datos
+	 */
+	private static final String DATABASE_DRIVER = "org.postgresql.Driver";
+	private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres";
+	private static final String DATABASE_USERNAME = "Empleado";
+	private static final String DATABASE_PASSWORD = "Empleado";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			// Leemos el fichero de URLs de datos
+			LOGGER.info("Leyendo fichero de URLs de datos");
+			List<String> dataUrlLines = FileUtils.readLines(new File(DATA_URLS_FILE));
+
+			// Descargamos las URLs de datos y las guardamos en ficheros en disco
+			for (String dataUrlLine : dataUrlLines)
+			{
+				if (!dataUrlLine.startsWith(CCOMENT))
+				{
+
+					String[] dataUrlLineTokens = dataUrlLine.split(CSEPARADOR);
+
+					String mercado = dataUrlLineTokens[0];
+					String bolsa = dataUrlLineTokens[1];
+					String indice = dataUrlLineTokens[2];
+					String ticker = dataUrlLineTokens[3];
+					String dataUrl = dataUrlLineTokens[4];
+
+					dataUrl = dataUrl.replaceAll(CFECINI, "2015-01-01");
+					dataUrl = dataUrl.replaceAll(CFECFIN, "2016-01-01");
+
+					LOGGER.info("Descargando URL [" + dataUrl + "]");
+					if (USE_PROXY)
+					{
+						descargarFicheroConProxy(mercado, bolsa, indice, ticker, dataUrl);
+					}
+					else
+					{
+						descargarFicheroSinProxy(mercado, bolsa, indice, ticker, dataUrl);
+					}
+
+				}
+
+			}
+
+			// Leemos los ficheros de datos descargados
+			Collection<File> dataFileList = FileUtils.listFiles(new File(TMP_DATA_FILE_PATH), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+			for (File dataFile : dataFileList)
+			{
+				LOGGER.info("dataFile: " + dataFile.getAbsolutePath());
+			}
+
+			LOGGER.info("Abriendo conexión a base de datos");
+			Connection connection = getConnection();
+			LOGGER.info("connection: " + connection.isClosed());
+		}
+		catch (Exception e)
+		{
+			LOGGER.error("ERROR", e);
+		}
+
+	}
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
+	private static Connection getConnection() throws Exception
+	{
+		try
+		{
+			Class.forName(DATABASE_DRIVER);
+			return DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+		}
+		catch (Exception e)
+		{
+			LOGGER.error("Error abriendo conexión a base de datos", e);
+			throw e;
+		}
+	}
+
+	/**
+	 * @param dataUrl
+	 * @param index
+	 */
+	private static void descargarFicheroConProxy(String mercado, String bolsa, String indice, String ticker, String dataUrl)
+	{
+		try
+		{
+			CredentialsProvider credsProvider = new BasicCredentialsProvider();
+			credsProvider.setCredentials(new AuthScope(PROXY_URL, PROXY_PORT), new UsernamePasswordCredentials(PROXY_USERNAME, PROXY_PASSWORD));
+			CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+			HttpHost proxy = new HttpHost(PROXY_URL, PROXY_PORT);
+			RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+			HttpGet httpget = new HttpGet(dataUrl);
+			httpget.setConfig(config);
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			FileUtils.copyInputStreamToFile(response.getEntity().getContent(), new File(TMP_DATA_FILE_PATH + TMP_DATA_FILE_PREFIX + mercado + CSEPARADOR + bolsa + CSEPARADOR + indice + CSEPARADOR + ticker));
+			response.close();
+			httpclient.close();
+		}
+		catch (Exception e)
+		{
+			LOGGER.error("Error descargando URL [" + dataUrl + "]", e);
+		}
+	}
+
+	/**
+	 * @param dataUrl
+	 * @param index
+	 */
+	private static void descargarFicheroSinProxy(String mercado, String bolsa, String indice, String ticker, String dataUrl)
+	{
+		throw new UnsupportedOperationException("Operación no implementada");
+	}
+
+}
