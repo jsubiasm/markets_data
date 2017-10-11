f09bfa1a2baa0a4c18e665c4594b59856749f432
jsubiasm <jsubiasm@yahoo.es>
2017-10-11 12:59:59 +0200

    zipeado el export de base de datos y modificado el cmd

--- ETL/run.cmd
+++ ETL/run.cmd
@@ -5,29 +5,42 @@ set M2_HOME=C:\_PELAYO\Software\Maven\apache-maven-3.3.9
 PATH=%JAVA_HOME%\bin;%M2_HOME%\bin
 
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2000-01-01 > 2000-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2000-07-01 > 2000-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2001-01-01 > 2001-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2001-07-01 > 2001-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2002-01-01 > 2002-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2002-07-01 > 2002-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2003-01-01 > 2003-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2003-07-01 > 2003-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2004-01-01 > 2004-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2004-07-01 > 2004-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2005-01-01 > 2005-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2005-07-01 > 2005-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2006-01-01 > 2006-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2006-07-01 > 2006-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2007-01-01 > 2007-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2007-07-01 > 2007-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2008-01-01 > 2008-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2008-07-01 > 2008-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2009-01-01 > 2009-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2009-07-01 > 2009-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2010-01-01 > 2010-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2010-07-01 > 2010-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2011-01-01 > 2011-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2011-07-01 > 2011-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2012-01-01 > 2012-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2012-07-01 > 2012-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2013-01-01 > 2013-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2013-07-01 > 2013-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2014-01-01 > 2014-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2014-07-01 > 2014-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2015-01-01 > 2015-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2015-07-01 > 2015-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2016-01-01 > 2016-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2016-07-01 > 2016-07-01.log
 REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-01-01 > 2017-01-01.log
+REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-07-01 > 2017-07-01.log
 
-REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2016-01-01
-REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2016-07-01
-REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-01-01
-REM java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-07-01
-
-java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-01-01
+java -jar .\target\ETL-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2017-07-01
 
 pause
