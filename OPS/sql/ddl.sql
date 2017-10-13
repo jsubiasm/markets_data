8e60a87cecf1dd94beac38bea296f37b8db8a73b
jsubiasm <jsubiasm@yahoo.es>
2017-10-13 12:42:35 +0200

    Renombrado fichero DDL

--- OPS/sql/ddl.sql
+++ OPS/sql/ddl.sql
@@ -1,5 +1,52 @@
 --
--- CLONACIÓN DE LA TABLA MERCADOS DESTINADA A REALIZAR CONSULTAS CON SUBCONJUNTOS DE DATOS DE LA TABLA MERCADOS
+-- TABLA MERCADOS
+--
+CREATE TABLE public.mercados (
+	mercado varchar NOT NULL,
+	bolsa varchar NOT NULL,
+	indice varchar NOT NULL,
+	ticker varchar NOT NULL,
+	fecha date NOT NULL,
+	maximo numeric NOT NULL,
+	minimo numeric NOT NULL,
+	cierre numeric NOT NULL,
+	volumen numeric NOT NULL
+)
+WITH (
+	OIDS=FALSE
+) ;
+create
+    index mercados_bolsa_idx on
+    mercados
+        using btree(bolsa) ;
+create
+    index mercados_fecha_idx on
+    mercados
+        using btree(fecha) ;
+create
+    index mercados_indice_idx on
+    mercados
+        using btree(indice) ;
+create
+    index mercados_mercado_idx on
+    mercados
+        using btree(mercado) ;
+create
+    index mercados_ticker_idx on
+    mercados
+        using btree(ticker) ;
+create
+    unique index mercados_unique_idx on
+    mercados
+        using btree(
+        mercado,
+        bolsa,
+        indice,
+        ticker,
+        fecha
+    ) ;
+--
+-- CLONACIÓN DE LA TABLA MERCADOS DESTINADA A REALIZAR CONSULTAS CON SUBCONJUNTOS DE LA TABLA MERCADOS
 --
 CREATE TABLE public.tmp_data (
 	mercado varchar NOT NULL,
@@ -46,7 +93,7 @@ create
         fecha
     ) ;
 --
--- INSERTA DATOS EN TMP_DATA A PARTIR DE MERCADOS
+-- INSERTA DATOS EN TMP_DATA A PARTIR DE MERCADOS FILTRANDO POR FECHAS
 --
 delete from public.tmp_data;
 insert into public.tmp_data select * from public.mercados where fecha > '2015-01-01' and fecha < '2015-12-31'; 
