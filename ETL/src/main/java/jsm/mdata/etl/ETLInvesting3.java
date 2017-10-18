567bf4b1385030b9d4bde8135093e1e71243f681
jsubiasm <jsubiasm@yahoo.es>
2017-10-18 16:59:47 +0200

    investing3

---
+++ ETL/src/main/java/jsm/mdata/etl/ETLInvesting3.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package jsm.mdata.etl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Empleado
+ *
+ */
+public class ETLInvesting3 extends ETLBase
+{
+
+	/**
+	 * Logger
+	 */
+	private final static Logger LOGGER = LoggerFactory.getLogger(ETLInvesting3.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+
+	}
+
+}
