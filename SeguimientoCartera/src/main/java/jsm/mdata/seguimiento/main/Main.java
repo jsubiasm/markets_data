bfcc86548d6995aa1a1e72abdfdfb0bf88621c32
jsubiasm <jsubiasm@LES006514.corp.capgemini.com>
2021-01-26 14:37:44 +0100

    Modificaciones base datos prueba

--- SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
+++ SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * 
  * @author JSM
  *
  */
@@ -45,47 +46,18 @@ public class Main
 	}
 
 	/**
-	 * @throws Exception
+	 * @throws Throwable
 	 */
 	private static void holaMundoDerbyEmbedded() throws Throwable
 	{
-		LOGGER.info("run()");
-
 		String dbUrl = "jdbc:derby:C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\derby\\prueba";
 		Connection conn = DriverManager.getConnection(dbUrl);
 		Statement stmt = conn.createStatement();
-
-		// drop table
-		// stmt.executeUpdate("Drop Table users");
-
-		// create table
-		// stmt.executeUpdate("Create table users (id int primary key, name varchar(30))");
-		// LOGGER.info("Tabla users creada");
-
-		// query
-		ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-		// print out query result
-		int secuencial = 1;
-		while (rs.next())
-		{
-			secuencial++;
-		}
-
-		// insert 2 rows
-		stmt.executeUpdate("insert into users values (" + (secuencial++) + ",'tom')");
-		stmt.executeUpdate("insert into users values (" + (secuencial++) + ",'peter')");
-		LOGGER.info("Dos filas insertadas");
-
-		// query
-		rs = stmt.executeQuery("SELECT * FROM users");
-
-		// print out query result
+		ResultSet rs = stmt.executeQuery("SELECT * FROM USERS");
 		while (rs.next())
 		{
 			LOGGER.info("row {id: " + rs.getInt("id") + ", name: " + rs.getString("name") + "}");
 		}
-
 	}
 
 }
