5f22faa74cafd1f781c0a6d109d769562f62aaac
jsubiasm <jsubiasm@LES006514.corp.capgemini.com>
2021-01-26 14:12:02 +0100

    Modificados CMDs. Base datos Derby. Clase Main.

--- SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
+++ SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
@@ -3,6 +3,11 @@
  */
 package jsm.mdata.seguimiento.main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +31,7 @@ public class Main
 		try
 		{
 			LOGGER.info("INICIO");
-			run();
+			holaMundoDerbyEmbedded();
 			LOGGER.info("CORRECTO");
 		}
 		catch (Throwable t)
@@ -42,9 +47,45 @@ public class Main
 	/**
 	 * @throws Exception
 	 */
-	private static void run() throws Throwable
+	private static void holaMundoDerbyEmbedded() throws Throwable
 	{
 		LOGGER.info("run()");
+
+		String dbUrl = "jdbc:derby:C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\derby\\prueba;create=true";
+		Connection conn = DriverManager.getConnection(dbUrl);
+		Statement stmt = conn.createStatement();
+
+		// drop table
+		// stmt.executeUpdate("Drop Table users");
+
+		// create table
+		// stmt.executeUpdate("Create table users (id int primary key, name varchar(30))");
+		// LOGGER.info("Tabla users creada");
+
+		// query
+		ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+		// print out query result
+		int secuencial = 1;
+		while (rs.next())
+		{
+			secuencial++;
+		}
+
+		// insert 2 rows
+		stmt.executeUpdate("insert into users values (" + (secuencial++) + ",'tom')");
+		stmt.executeUpdate("insert into users values (" + (secuencial++) + ",'peter')");
+		LOGGER.info("Dos filas insertadas");
+
+		// query
+		rs = stmt.executeQuery("SELECT * FROM users");
+
+		// print out query result
+		while (rs.next())
+		{
+			LOGGER.info("row {id: " + rs.getInt("id") + ", name: " + rs.getString("name") + "}");
+		}
+
 	}
 
 }
