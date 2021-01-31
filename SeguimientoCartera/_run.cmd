2b3dc5ba25d80c013b29f4446cade392e72bceb4
jsubiasm <jsubiasm@LES006514.corp.capgemini.com>
2021-01-31 16:32:48 +0100

    validación movimientos

--- SeguimientoCartera/_run.cmd
+++ SeguimientoCartera/_run.cmd
@@ -3,6 +3,6 @@
 set JAVA_HOME=C:\_JSM\SeguimientoCartera\01_Software\jdk-15.0.2_windows-x64_bin\jdk-15.0.2
 PATH=%JAVA_HOME%\bin;%M2_HOME%\bin
 
-java -jar .\target\SeguimientoCartera-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar .\target\SeguimientoCartera-0.0.1-SNAPSHOT-jar-with-dependencies.jar > run.log
 
 pause
--- SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
+++ SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
@@ -3,11 +3,14 @@
  */
 package jsm.mdata.seguimiento.main;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,15 +29,24 @@ public class Main
 	private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	/**
+	 * 
+	 */
+	private static final String DB_PROTOCOL = "jdbc:derby:";
+
+	/**
+	 * 
+	 */
+	private static final String DB_PATH = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\derby\\seguimiento_cartera";
+
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
 		try
 		{
-			LOGGER.info("INICIO");
-			holaMundoDerbyEmbedded();
-			LOGGER.info("CORRECTO");
+			LOGGER.info("INICIO PROCESO");
+			validateMovimientos();
 		}
 		catch (Throwable t)
 		{
@@ -42,32 +54,99 @@ public class Main
 		}
 		finally
 		{
-			LOGGER.info("FIN");
+			LOGGER.info("FIN PROCESO");
 		}
+	}
+
+	/**
+	 * @return
+	 * @throws Throwable
+	 */
+	private static Connection getConnection() throws Throwable
+	{
+		String dbUrl = DB_PROTOCOL + DB_PATH;
+		Connection connection = DriverManager.getConnection(dbUrl);
+		connection.setAutoCommit(false);
+		return connection;
 	}
 
 	/**
 	 * @throws Throwable
 	 */
-	private static void holaMundoDerbyEmbedded() throws Throwable
+	private static void validateMovimientos() throws Throwable
 	{
-		String dbUrl = "jdbc:derby:C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\derby\\prueba;create=true";
-		Connection conn = DriverManager.getConnection(dbUrl);
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM USERS");
-		while (rs.next())
-		{
-			LOGGER.info("row {id: " + rs.getInt("id") + ", name: " + rs.getString("name") + "}");
-		}
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
 		try
 		{
-			DriverManager.getConnection("jdbc:derby:;shutdown=true");
-		}
-		catch (SQLException se)
-		{
-			if (se.getErrorCode() != 50000 || !"XJ015".equals(se.getSQLState()))
+			LOGGER.info("Abriendo connection");
+			connection = getConnection();
+			LOGGER.info("Abriendo statement");
+			statement = connection.createStatement();
+			LOGGER.info("Abriendo resultSet");
+			resultSet = statement.executeQuery("SELECT MOVIMIENTO_ID, PRODUCTO_ID, COMPRA_VENTA, FECHA, NUMERO_TITULOS, PRECIO_TITULO, COMISION, TOTAL, COMERCIALIZADOR, MERCADO from MASTER_MOVIMIENTOS");
+			while (resultSet.next())
 			{
-				throw se;
+				Integer movimientoId = resultSet.getInt("MOVIMIENTO_ID");
+				String produtoId = resultSet.getString("PRODUCTO_ID");
+				String compraVenta = resultSet.getString("COMPRA_VENTA");
+				Date fecha = resultSet.getDate("FECHA");
+				BigDecimal numeroTitulos = resultSet.getBigDecimal("NUMERO_TITULOS");
+				BigDecimal precioTitulo = resultSet.getBigDecimal("PRECIO_TITULO");
+				BigDecimal comision = resultSet.getBigDecimal("COMISION");
+				BigDecimal total = resultSet.getBigDecimal("TOTAL");
+				String comercializador = resultSet.getString("COMERCIALIZADOR");
+				String mercado = resultSet.getString("MERCADO");
+				LOGGER.info("-------------------------------------------------");
+				LOGGER.info("movimientoId [" + movimientoId + "]");
+				LOGGER.info("produtoId [" + produtoId + "]");
+				LOGGER.info("compraVenta [" + compraVenta + "]");
+				LOGGER.info("fecha [" + fecha + "]");
+				LOGGER.info("numeroTitulos [" + numeroTitulos + "]");
+				LOGGER.info("precioTitulo [" + precioTitulo + "]");
+				LOGGER.info("comision [" + comision + "]");
+				LOGGER.info("total [" + total + "]");
+				LOGGER.info("comercializador [" + comercializador + "]");
+				LOGGER.info("mercado [" + mercado + "]");
+				BigDecimal totalCalculado = numeroTitulos.multiply(precioTitulo).add(comision).setScale(2, RoundingMode.HALF_UP);
+				if (totalCalculado.equals(total.setScale(2, RoundingMode.HALF_UP)))
+				{
+					LOGGER.info("MOVIMIENTO VALIDADO");
+				}
+				else
+				{
+					LOGGER.info("MOVIMIENTO NO VALIDADO [" + totalCalculado + "] [" + total + "]");
+				}
+			}
+			LOGGER.info("Commit connection");
+			connection.commit();
+		}
+		catch (Throwable t)
+		{
+			LOGGER.error("ERROR", t);
+			LOGGER.info("Rollback connection");
+			connection.rollback();
+		}
+		finally
+		{
+			LOGGER.info("Cerrando resultSet");
+			resultSet.close();
+			LOGGER.info("Cerrando statement");
+			statement.close();
+			LOGGER.info("Cerrando connection");
+			connection.close();
+			LOGGER.info("Cerrando DB");
+			try
+			{
+				DriverManager.getConnection("jdbc:derby:;shutdown=true");
+			}
+			catch (SQLException se)
+			{
+				if (se.getErrorCode() != 50000 || !"XJ015".equals(se.getSQLState()))
+				{
+					throw se;
+				}
 			}
 		}
 	}
