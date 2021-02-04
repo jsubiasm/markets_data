dd510eefb4e218779584b3f7eb63e11e6521d0ce
jsubiasm <jsubiasm@LES006514.corp.capgemini.com>
2021-02-04 23:37:57 +0100

    Modificado run para mostrar log por pantalla

--- SeguimientoCartera/_run.cmd
+++ SeguimientoCartera/_run.cmd
@@ -3,6 +3,7 @@
 set JAVA_HOME=C:\_JSM\SeguimientoCartera\01_Software\jdk-15.0.2_windows-x64_bin\jdk-15.0.2
 PATH=%JAVA_HOME%\bin;%M2_HOME%\bin
 
-java -jar .\target\SeguimientoCartera-0.0.1-SNAPSHOT-jar-with-dependencies.jar > run.log
+@rem java -jar .\target\SeguimientoCartera-0.0.1-SNAPSHOT-jar-with-dependencies.jar > run.log
+java -jar .\target\SeguimientoCartera-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
 pause
