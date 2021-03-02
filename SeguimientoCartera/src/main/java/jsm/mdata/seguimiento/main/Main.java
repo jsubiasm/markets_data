6ab667585bae1c19bfb9918627e33a079d79b588
jsubiasm <jsubiasm@LES006514.corp.capgemini.com>
2021-03-02 18:01:34 +0100

    Reagrupados sql, tpl, y html en una misma carpeta llamada resources

--- SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
+++ SeguimientoCartera/src/main/java/jsm/mdata/seguimiento/main/Main.java
@@ -56,8 +56,8 @@ public class Main
 	 * 
 	 */
 	private static final String DB_PATH = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\derby\\seguimiento_cartera";
-	private static final String HTML_TEMPLATE = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\html\\seguimiento_cartera.template.html";
-	private static final String HTML_OUTPUT = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\output\\seguimiento_cartera.output.html";
+	private static final String HTML_TEMPLATE = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\resources\\02_seguimiento_cartera.template.tpl";
+	private static final String HTML_OUTPUT = "C:\\_JSM\\SeguimientoCartera\\03_Fuentes\\markets_data\\SeguimientoCartera\\resources\\03_seguimiento_cartera.output.html";
 
 	/**
 	 * @return
